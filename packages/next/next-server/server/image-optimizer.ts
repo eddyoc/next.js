999044be91e0ba28d0643ab7ee767225706f6217
eddy@macritchie.io <8Chaos0013>
2021-02-23 18:18:22 +0800

    remove cache-control header overwrites from image optimiser

--- packages/next/next-server/server/image-optimizer.ts
+++ packages/next/next-server/server/image-optimizer.ts
@@ -143,7 +143,7 @@ export async function imageOptimizer(
       const contentType = getContentType(extension)
       const fsPath = join(hashDir, file)
       if (now < expireAt) {
-        res.setHeader('Cache-Control', 'public, max-age=0, must-revalidate')
+        // res.setHeader('Cache-Control', 'public, max-age=0, must-revalidate')
         if (sendEtagResponse(req, res, etag)) {
           return { finished: true }
         }
@@ -296,7 +296,7 @@ function sendResponse(
   buffer: Buffer
 ) {
   const etag = getHash([buffer])
-  res.setHeader('Cache-Control', 'public, max-age=0, must-revalidate')
+  // res.setHeader('Cache-Control', 'public, max-age=0, must-revalidate')
   if (sendEtagResponse(req, res, etag)) {
     return
   }
